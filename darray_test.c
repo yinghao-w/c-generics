61eaf22e6e9cce6c6af30bd63fd4bdbe6cd3c244
yinghao <>
2025-09-15 23:59:44 +0100

    tests integer elements, destroy issue
    problem when destroying the darray, cannot free darray -> data

---
+++ darray_test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <threads.h>
+#include "void_darray.h"
+
+
+int main(void) {
+
+	Darray *test_darray = init(2);
+
+	int *p[100];
+	for (int i=0; i < 100; i++) {
+		p[i] = malloc(sizeof(p[i])); 
+		*p[i] = 2*i;
+		append(p[i], test_darray);
+	}
+
+
+	for (int i = 0; i < size(test_darray); i++) {
+		printf("%d\n", *(int *)get(i, test_darray));
+	}
+
+	destroy(test_darray);
+
+	return 0;
+}
