46fcde3d1bd8acb6b849990f14e964909e71afb2
yinghao <>
2025-09-16 12:43:54 +0100

    tests with struct elements

--- code_gen_test.c
+++ code_gen_test.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include "code_gen.h"
 
-INIT(int, i);
+struct point {
+	int a;
+	int b;
+};
+typedef struct point point;
+
+void init_pt (point *pt, int a, int b) {
+	pt -> a = a;
+	pt -> b = b;
+}
+
+INIT(point, pt);
 
 int main(void) {
 
-	idarray *p = icreate(4);
+	point q;
+
+	ptdarray *p = ptcreate(4);
 	for (int i=0; i<4; i++) {
-		p -> data[i] = 10 * i;
+		init_pt(p -> data+i, 3 *i, 7*i);
 	};
 	for (int i=0; i<4; i++) {
-		printf("%d\n", p -> data[i]);
+		printf("%d\n", p -> data[i].b);
 	};
 
 	printf("");
