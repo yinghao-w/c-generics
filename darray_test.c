9b06867188e674fba3b0df9cf06fd82821b61b70
yinghao <>
2025-09-17 15:09:51 +0100

    successful double free test

--- darray_test.c
+++ darray_test.c
@@ -7,24 +7,27 @@ int main(void) {
 
 	Darray *test_darray = create(2);
 
-	int *p[100];
-	for (int i=0; i < 100; i++) {
-		p[i] = malloc(sizeof(p[i])); 
-		*p[i] = 2*i;
-		append(p[i], test_darray);
-	}
+	/* int *p[100]; */
+	/* for (int i=0; i < 100; i++) { */
+	/* 	p[i] = malloc(sizeof(p[i])); */ 
+	/* 	*p[i] = 2*i; */
+	/* 	append(p[i], test_darray); */
+	/* } */
 
 	int *q = malloc(sizeof(q));
 
-	*q = 999999;
+	*q = 9999;
 
-	insert(q, 80, test_darray);
+	for (int i=0; i<100; i++) {
+		append(q, test_darray);
+	}
+
 
 	for (int i = 0; i < size(test_darray); i++) {
 		printf("%d\n", *(int *)get(i, test_darray));
 	}
 
-	printf("%d\t%d\t%d\n", size(test_darray), is_in(p[45], test_darray), is_in(test_darray, test_darray));
+	printf("%d\t%d\t%d\n", size(test_darray), is_in(q, test_darray), is_in(test_darray, test_darray));
 
 	destroy(test_darray);
 
