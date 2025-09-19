67eed1abc7ec9ce60aad54b01b44ab0c5256dcc4
yinghao <>
2025-09-19 21:54:37 +0100

    Update test

--- test.c
+++ test.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "fat_pointer.h"
 
+#define FUNC(x) 
+
 int main(void) {
-	int *arr = NULL;
 
-	INIT(arr);
+	int *a = NULL;
+	int Q = 99999999;
 
+	for (int i = 0; i<Q; i++) {
+		fp_push(2 * i, a);
+	};
+	printf("%d\t%d\n", FP_HEADER(a) -> length, FP_HEADER(a) -> capacity);
 
+	for (int i = 0; i<Q; i++) {
+		fp_pop(a);
+	};
 
+	int A = 1;
+	int B = FUNC(a);
 
 	return 0;
 }
+ 	
