393a5cba08d4a6593d4d2a7e0737fd3825c88250
yinghao <>
2025-09-20 14:21:32 +0100

    Make the test applicable with all implementations

--- calc_test.c
+++ calc_test.c
@@ -4,77 +4,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "code_gen.h"
-CG_INIT(int, i)
-
-/* Create uniform stack interface */
-
-#define CALC_PUSH i_push
-#define CALC_POP i_pop
-#define CALC_CREATE() i_create(2)
-#define CALC_DESTROY i_destroy
-#define CALC_STACK i_stack
+#include "fat_pointer.h"
 
 /* Calculator */
 
 #define NUMBER 0
 
 char type(char *token) {
+
 	return isdigit(token[0]) ? NUMBER : token[0];
 
 }
 
-int calc(char *input, CALC_STACK *stack) { 
-
-	char *token;
-	token = strtok(input, " ");
-	while (token != NULL) {
-		int temp1;
-		int temp2;
-		switch (type(token)) {
-		int x;
-		case NUMBER:
-			CALC_PUSH(atoi(token), stack);
-			break;
-		case '+':
-			temp1 = CALC_POP(stack) + CALC_POP(stack);
-			CALC_PUSH(temp1, stack);
-			break;
-		case '-':
-			/* subtracts top of stack from second top */
-			temp1 = CALC_POP(stack);
-			temp2 = CALC_POP(stack);
-			temp2 -= temp1;
-			CALC_PUSH(temp2, stack);
-			break;
-		case '*':
-			temp1 = CALC_POP(stack) * CALC_POP(stack);
-			CALC_PUSH(temp1, stack);
-			break;
-		case '/':
-			temp1 = CALC_POP(stack);
-			if (temp1 == 0) {
-				printf("Error: division by 0\n");
-				fprintf(stderr, "Error: division by 0\n");
-			}
-			temp2 = CALC_POP(stack);
-			temp2 /= temp1;
-			CALC_PUSH(temp2, stack);
-			break;
-		default:
-			break;
+/* Macro to generate calc function for each stack implementation */
+#define MAKE_CALC(CALC_CREATE, CALC_PUSH, CALC_POP, CALC_DESTROY, PREFIX)	\
+		int PREFIX##_calc(const char *input) {								\
+																			\
+			/* Note: strtok modifies its input_copy, so to call calc on
+			 * argv[1] multiple times, a copy of argv[1] should be passed */\
+			int input_length = strlen(input);								\
+			char input_copy[input_length];									\
+			strncpy(input_copy, input, input_length);						\
+																			\
+			CALC_CREATE														\
+			char *token;													\
+			token = strtok(input_copy, " ");								\
+																			\
+			while (token != NULL) {											\
+				int temp1;													\
+				int temp2;													\
+				switch (type(token)) {										\
+				int x;														\
+				case NUMBER:												\
+					CALC_PUSH(atoi(token), stack);							\
+					break;													\
+				case '+':													\
+					temp1 = CALC_POP(stack) + CALC_POP(stack);				\
+					CALC_PUSH(temp1, stack);								\
+					break;													\
+				case '-':													\
+					/* subtracts top of stack from second top */			\
+					temp1 = CALC_POP(stack);								\
+					temp2 = CALC_POP(stack);								\
+					temp2 -= temp1;											\
+					CALC_PUSH(temp2, stack);								\
+					break;													\
+				case '*':													\
+					temp1 = CALC_POP(stack) * CALC_POP(stack);				\
+					CALC_PUSH(temp1, stack);								\
+					break;													\
+				case '/':													\
+					temp1 = CALC_POP(stack);								\
+					if (temp1 == 0) {										\
+						printf("Error: division by 0\n");					\
+						fprintf(stderr, "Error: division by 0\n");			\
+					}														\
+					temp2 = CALC_POP(stack);								\
+					temp2 /= temp1;											\
+					CALC_PUSH(temp2, stack);								\
+					break;													\
+				default:													\
+					break;													\
+				}															\
+				token = strtok(NULL, " ");									\
+			}																\
+			int result = CALC_POP(stack);									\
+			CALC_DESTROY(stack);											\
+			return result;													\
 		}
-		token = strtok(NULL, " ");
-	}
-	return CALC_POP(stack);
-}
+
+/* Generate calc functions */
+
+CG_INIT(int, i)
+#define CALC_CREATE i_stack *stack = i_create(2);
+MAKE_CALC(CALC_CREATE, i_push, i_pop, i_destroy, cg)
+#undef CALC_CREATE
+
+#define CALC_CREATE int *stack = NULL;
+MAKE_CALC(CALC_CREATE, fp_push, fp_pop, fp_destroy, fp)
+#undef CALC_CREATE
 
 int main(int argc, char *argv[]) {
-	CALC_STACK *stack = CALC_CREATE();
-	int result = calc(argv[1], stack);
-	printf("Result: %d\n", result);
-	CALC_DESTROY(stack);
+	
+	int fp_result = fp_calc(argv[1]);
+	int cg_result = cg_calc(argv[1]);
+
+	assert(fp_result == cg_result);
+
+	printf("Results:\nCG: %d\nFP: %d\n", cg_result, fp_result);
 
 	return 0;
 }
