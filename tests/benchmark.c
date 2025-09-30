4ae4920c55a9cc7f04c6bc280ba5e611a4485f16
yinghao <231103865+wnagmeister@users.noreply.github.com>
2025-09-30 14:01:39 +0100

    Update benchmark

--- tests/benchmark.c
+++ tests/benchmark.c
@@ -4,17 +4,53 @@
 #include <stdio.h>
 #include <time.h>
 
+#include "code_gen.h"
+CG_INIT(int, i)
+
+#include "fat_pointer.h"
+#include "void.h"
+
 #include "../../klib/kvec.h"
 
 #define STB_DS_IMPLEMENTATION
 #include "../../stb/stb_ds.h"
 
-#define T vec_int, int, (c_use_cmp)
-#include "../../STC/include/stc/vec.h"
-
 void speed_test(void) {
   int M = 10, N = 20000000, i, j;
   clock_t t;
+
+  t = clock();
+  for (i = 0; i < M; ++i) {
+    i_darray *darray = i_create(2);
+    for (j = 0; j < N; ++j)
+      i_push(j, darray);
+    for (j = 0; j < N; ++j)
+      i_pop(darray);
+    i_destroy(darray);
+  }
+  printf("code_gen.h : %.3f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+
+  t = clock();
+  for (i = 0; i < M; ++i) {
+    int *darray = NULL;
+    for (j = 0; j < N; ++j)
+      fp_push(j, darray);
+    for (j = 0; j < N; ++j)
+      fp_pop(darray);
+    fp_destroy(darray);
+  }
+  printf("fat_pointer.h : %.3f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+
+  t = clock();
+  for (i = 0; i < M; ++i) {
+    V_Darray *darray = v_create(2, sizeof(int));
+    for (j = 0; j < N; ++j)
+      v_push(&j, darray);
+    for (j = 0; j < N; ++j)
+      v_pop(NULL, darray);
+    v_destroy(darray);
+  }
+  printf("void.h : %.3f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
 
   t = clock();
   for (i = 0; i < M; ++i) {
@@ -26,7 +62,7 @@ void speed_test(void) {
       kv_pop(darray);
     kv_destroy(darray);
   }
-  printf("KLIB KVEC: %.3f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+  printf("kvec.h %.3f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
 
   t = clock();
   for (i = 0; i < M; ++i) {
@@ -37,7 +73,7 @@ void speed_test(void) {
       arrpop(darray);
     arrfree(darray);
   }
-  printf("STB_DS: %.3f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+  printf("stb_ds.h: %.3f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
 }
 
 int main(void) {
